429849c16551499510eb0a86029ea7620e5a8cfa
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-15 01:02:02 +0700

    Modified config

--- app/BE/model_config.py
+++ app/BE/model_config.py
@@ -76,16 +76,6 @@ MODEL_CONFIGS = [
         "arch_fn": lsnet_b
     },
     {
-        "id": "lsnet_tiny_detection",
-        "type": "detection",
-        "name": "LSNet Tiny Detection (RetinaNet)",
-        "config_path": os.path.join(current_dir, "..", "..", "lsnet", "detection", "configs", "retinanet_lsnet_t_fpn_1x_coco.py"),
-        "weights_path": os.path.join(current_dir, "pretrained", "retinanet_lsnet_t_finetuned.pth"),
-        "classes_path": os.path.join(current_dir, "pretrained", "vietnamese_food_classes.txt"),
-        "weights_url": "", # Add URL if available
-        "classes_url": "https://huggingface.co/MatchaMacchiato/LSNet_VietnameseFood/resolve/main/vietnamese_food_classes.txt?download=true",
-    },
-    {
         "id": "retinanet_lsnet_t",
         "type": "detection",
         "name": "LSNet Tiny Detection (RetinaNet)",
@@ -108,7 +98,7 @@ MODEL_CONFIGS = [
     {
         "id": "retinanet_lsnet_b",
         "type": "detection",
-        "name": "LSNet Big Detection (RetinaNet)",
+        "name": "LSNet Base Detection (RetinaNet)",
         "config_path": os.path.join(current_dir, "..", "..", "lsnet", "detection", "configs", "retinanet_lsnet_b_fpn_1x_food_coco.py"),
         "weights_path": os.path.join(current_dir, "pretrained", "retinanet_lsnet_b_finetuned.pth"),
         "classes_path": os.path.join(current_dir, "pretrained", "vietnamese_food_classes.txt"),
